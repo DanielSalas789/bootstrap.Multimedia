fa4764ef430679457cdce995181b5888345b3597
Daniel Eduardo Salas Cruz <salascruzdanieleduardo7@gmail.com>
2024-09-17 11:16:49 -0600

    modificacion navbar y padding

--- SCRIPTS/index.js
+++ SCRIPTS/index.js
@@ -1,5 +1,12 @@
-window.addEventListener('load', function () {
+document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.navbar').classList.add('show');
 });
 
-
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 2) { // Cambia el número según cuándo quieras que se aplique la transición
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
